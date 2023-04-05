88dd7653d968463fe7512bbbcd7a89f4752538ed
Mofeid Moshreky <mof_ton@yahoo.com>
2023-04-04 23:01:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+  return arr.slice(0, 2);
+};
+const returnLastTwoDrivers = (arr) => {
+  return arr.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (value) => {
+  return (fare) => value * fare;
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(arrayOfDrivers, returnDrivers) {
+  return returnDrivers(arrayOfDrivers);
+}
